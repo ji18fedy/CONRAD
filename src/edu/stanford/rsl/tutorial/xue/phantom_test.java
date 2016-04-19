2d430c74c8118897d2b8af4fdb8c44ec1748c54a
Xue Zhang <ji18fedy@stud.informatik.uni-erlangen.de>
2016-04-19 11:58:31 +0200

    phantom with 3 objects in it

--- src/edu/stanford/rsl/tutorial/xue/phantom_test.java
+++ src/edu/stanford/rsl/tutorial/xue/phantom_test.java
@@ -1,10 +1,44 @@
 package edu.stanford.rsl.tutorial.xue;
 
-public class phantom_test  {
+import edu.stanford.rsl.conrad.data.numeric.Grid2D;
+
+public class phantom_test extends Grid2D {
+
+	public phantom_test(int x, int y){
+		super(x,y);
+		float valueIntensity1;
+		float valueIntensity2;
+		float valueIntensity3;
+		valueIntensity1=0.8f;
+		valueIntensity2=0.4f;
+		valueIntensity3=0.1f;
+		this.setAtIndex(x, y, valueIntensity1);
+		for(int i=0;i<x/4;i++){
+			for(int j=0;j<y/4;j++){
+				this.setAtIndex(i, j, valueIntensity1);
+			}
+		}
+		
+		
+		for(int i=(x/2+x/4);i<x/2+2*x/4;i++){
+			for(int j=y/4;j<y/4+y/8;j++){
+				this.setAtIndex(i, j, valueIntensity2);
+			}
+		}
+		
+		for(int i=(x/4);i<x/2+x/4;i++){
+			for(int j=y/2+y/8;j<y/2+2*y/8;j++){
+				this.setAtIndex(i, j, valueIntensity3);
+			}
+		}
+		
+	}
+	//public phantom_test()
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		phantom_test yourPhantom = new phantom_test(512,512);
+		yourPhantom.show();
 	}
 
 }
